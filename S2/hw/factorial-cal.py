0eb4ee19a6b14923b484523f816a271dc3a2cfd4
minhduc9699 <30574677+minhduc9699@users.noreply.github.com>
2018-03-06 19:46:38 +0700

    Update factorial-cal.py

--- S2/hw/factorial-cal.py
+++ S2/hw/factorial-cal.py
@@ -1,10 +1,15 @@
 
+#method 1:
+
 '''
 from math import factorial
 num = int(input('enter a number: '))
 num = factorial(num)
 print(num)
 '''
+
+#menthod 2:
+
 num = int(input('enter a number: '))
 fac = 1
 for i in range(1, num + 1):
