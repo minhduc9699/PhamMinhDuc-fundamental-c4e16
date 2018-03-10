dbce99456e019f4033793302a68f0f684f1e46b0
minh duc <minhduc.096.99@gmail.com>
2018-03-10 15:59:29 +0700

    update hw

--- S3/hw/chan_cuu.py
+++ S3/hw/chan_cuu.py
@@ -3,8 +3,9 @@ sheep_size = [4, 47, 34, 346, 32, 25, 90]
 def shear():
     sheep_size_biggest = max(sheep_size)
     print('now my biggest sheep size is %d, lets shear it' % sheep_size_biggest)
-    sheep_size.remove(max(sheep_size))
-    sheep_size.append(8)
+    for index, item in enumerate(sheep_size):
+        if item == sheep_size_biggest:
+            sheep_size[index] = 8
     print('after shearing here my flock: ')
     print(sheep_size, sep=' ')
 
@@ -13,6 +14,7 @@ def grow(i):
     sheep_size = [x + i for x in sheep_size]
     print('One month has passed, now here my flock')
     print(sheep_size)
+
 def sell():
     total_size = sum(sheep_size)
     sell_money = total_size * 2
@@ -20,12 +22,14 @@ def sell():
     print('i would get', total_size, '* 2$ = ', sell_money,'$')
 
 
-
+print('*' * 20)
 print('hello, my name is Duc, and here is my flock: ')
 print(sheep_size)
 print('\n')
-for i in range(50,250,50):
-    grow(i)
+shear()
+print('\n')
+for i in range(4):
+    grow(50)
     shear()
     print('\n')
 sell()
