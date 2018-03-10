0ee1748cfdf14e75282600ee90b1724cd2f1fd09
minh duc <minhduc.096.99@gmail.com>
2018-03-10 16:45:13 +0700

    update hw

--- S3/hw/word_jumbel.py
+++ S3/hw/word_jumbel.py
@@ -1,13 +1,23 @@
 from random import *
 from getpass import getpass
 
-quizz = getpass('enter your quizz: ')
-char = list(quizz)
-shuffle(char)
-print(*char)
+quizz = []
+number_of_quizz = int(input('how many quizz do you want? '))
+for i in range(number_of_quizz):
+    word = getpass('enter your quizz: ')
+    quizz.append(word)
 
-key = input('enter your awnser: ')
-if key == quizz:
-    print('Bingo!!')
-else:
-    print(':(')
+playing = True
+while playing:
+    random_quizz = quizz[randint(0,len(quizz)-1)]
+    char = list(random_quizz)
+    shuffle(char)
+    print(*char)
+
+    key = input('enter your awnser: ')
+    if key == random_quizz:
+        print('Bingo!!')
+        print('Next quizz:')
+    else:
+        print(':(')
+        phaying = False
