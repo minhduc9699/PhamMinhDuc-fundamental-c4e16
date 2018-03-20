9b7190e14e04ee046811f7d2f0eae5c425786981
minh duc <minhduc.096.99@gmail.com>
2018-03-20 10:02:21 +0700

    update hw

--- lab_1/hw/c4e_blogs.py
+++ lab_1/hw/c4e_blogs.py
@@ -9,11 +9,15 @@ db = client.get_default_database()
 posts = db['posts']
 
 new_post = {
-    "title" : "Tuyên ngôn... học code",
-    "author" : "Minh Đức",
-    "content" : """Hỡi đồng bào cả nước,
-    tất cả mọi người sinh ra đều có quyền bình đẳng. Tạo hóa cho họ những quyền không ai có thể xâm phạm được;
-    trong những quyền ấy, có quyền được học code, quyền tự do viết code, và quyền compile code."""
+    "title" : "Tuyên ngôn... l4d2",
+    "author" : "game thủ",
+    "content" : """Tất cả mọi người sinh ra đều có quyền bình đẳng.
+    Tạo hóa cho họ những quyền không ai có thể xâm phạm được, trong những quyền ấy,
+    có quyền được charger đâm,
+    quyền tự do được smoker đá lưỡi,
+    quyền được cõng jockey,
+    quyền được hunter theo đuổi,
+    và quyền được witch âu yếm."""
 }
 
 posts.insert_one(new_post)
